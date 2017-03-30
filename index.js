23e9f68d73783f2ebf22c9a394761d6ce017e08b
Cameron Knight <ckknight@gmail.com>
2017-03-29 21:16:29 -0700

    Read the file as binary instead of utf8

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var bootstrap = fs.readFileSync(__dirname + '/output.js', 'utf8');
 module.exports = function() {
   var path = loaderUtils.interpolateName(this, '[path][name].[ext]', { context: this.options.context });
   // TODO: Figure out if there is a way to convert webpacks source param to Uint8 correctly
-  var buffer = fs.readFileSync(this.options.context + "/" + path);
+  var buffer = fs.readFileSync(this.options.context + "/" + path, 'binary');
   var out = "var buffer = new ArrayBuffer(" + buffer.length + ");";
   out += "var uint8 = new Uint8Array(buffer);";
   out += "uint8.set([";
