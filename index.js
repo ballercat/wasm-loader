549a3b8c9c39e194f890ee05b0def8fa692e7306
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-02-11 15:30:30 +0100

    feat: add DCE

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var fs = require('fs');
 var bootstrap = fs.readFileSync(__dirname + '/output.js', 'utf8');
+var loaderUtils = require('loader-utils');
+var wasmDCE = require('wasm-dce')
+
+var config = {dce: true};
 
 module.exports = function(buffer) {
+  var params = loaderUtils.parseQuery(this.resourceQuery);
+
+  if (params.dce === '1') {
+    // FIXME(sven): get the used exports
+    var usedExports = [];
+
+    buffer = wasmDCE(buffer, usedExports);
+  }
+
   var out = "var buffer = new ArrayBuffer(" + buffer.length + ");";
   out += "var uint8 = new Uint8Array(buffer);";
   out += "uint8.set([";
